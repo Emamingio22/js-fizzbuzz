43f46dc1841eb13803ad611419f78aa3322a7c4c
emamingio22 <emawankenoby02@gmail.com>
2023-03-30 16:37:40 +0200

    I made the main

--- main.js
+++ main.js
@@ -8,4 +8,29 @@ const row = document.querySelector('.row');
 const card = document.getElementsByClassName('.card');
 console.log(card);
 
+const container = document.querySelector('.container');
+
 /*CONTINUO CON IL CICLO*/
+
+for(let i = 1; i <= 100; i++){
+
+  const box = document.createElement('div');
+  box.className = 'box';
+
+  let output = i;
+
+
+  
+  if(!(i % 15)){
+    output = 'FizzBuzz';
+    box.classList.add('fizzbuzz');
+  }else if(!(i % 3)){
+    output = 'Fizz'
+    box.classList.add('fizz');
+  }else if(!(i % 5)){
+    output = 'Buzz'
+    box.classList.add('buzz');
+  }
+  box.append(output);
+  container.append(box);
+}
